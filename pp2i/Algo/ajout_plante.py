2be7f768cbe46b7d5e5807af24d0e2b792c21c65
leo.fornoff <leo.fornoff@telecomnancy.eu>
2022-10-25 21:27:25 +0200

    Algo : add ajout_plant working, starting creation of a class terrain

--- pp2i/Algo/ajout_plante.py
+++ pp2i/Algo/ajout_plante.py
@@ -1,28 +1,48 @@
 """
-Le but de ce script est d\'ajouter, si cela est possible, une plante, en fonction de sa taille, à l'endroit demandé et si cela est possible
+Le but de ce script est d\'ajouter, si cela est possible, une plante, en fonction de sa taille,
+à l'endroit demandé et si cela est possible
 """
+import numpy as np
 
-def modification_terrain(terrain : array, taille : int, position : tuple):
 
-    return
+def modification_terrain(terrain: np.array, taille_plante: int, position: tuple, id_plante: int):
+    x, y = position
+    colonne_legume = np.full_like(np.zeros(taille_plante), id_plante)
+    colonne_vide = np.full_like(np.zeros(taille_plante), -1)
+    for k in range(taille_plante):
+        if np.array_equal(
+                terrain[y + k, x:x + taille_plante],
+                colonne_vide
+        ):
+            terrain[y + k, x:x + taille_plante] = colonne_legume
+        else:
+            return False
+    return True, terrain
 
-def ajout_plante(terrain : array, taille_plante : int, position : tuple):
+
+def ajout_plante(terrain: np.array, taille_plante: int, position: tuple, id_plante: int, echelle=1):
     """
-    :param terrain: matrice représentant le terrain
+    :param terrain: matrice représentant le terrain, que l'on suppose non nulle
+    car créé par la fonction de création de terrain
     :param taille_plante: on représente une plante par un carré de coté taille_plante, taille plante est un entier
     :param position: position du coin en haut à gauche de la plante, l'indice du jardin commence en 0,0
+    :param id_plante: numéro d'identification de la plante (entier positif)
+    :param echelle: xargs de valeur par défault
     :return:
     """
+    taille_plante = int(taille_plante // echelle)
+    print(taille_plante)
     if len(position) != 2:
         msg = 'Les coordonnées de position ne se pas correcte, cf taille tuple'
         return terrain, False, msg
     if position[0] < 0 or position[1] < 0:
-        msg = 'position impossible car l\'une des coordonées est négative'
+        msg = 'position impossible car l\'une des coordonées est négative '
         return terrain, False, msg
-    if position[0] + taille_plante >= len(terrain[0]) or position[1] + taille_plante >= len(terrain[1]):
+    if position[0] + taille_plante > len(terrain[0]) or position[1] + taille_plante > len(terrain[1]):
         msg = 'La plante ne rentre pas dans le jardin à cette postion'
         return terrain, False, msg
-
-
-
-
+    terrain_modifie = modification_terrain(terrain, taille_plante, position, id_plante)
+    if terrain_modifie[0]:
+        return terrain_modifie[1]
+    else:
+        return terrain
---
+++ pp2i/Algo/class_terrain.py
@@ -0,0 +1,5 @@
+class terrain:
+    """
+    il faudra définir ici une class terrain
+    afin de facilité l'utilisation de toutes les fonctions sous jacentes à l'utilisation d'un terrain
+    """
