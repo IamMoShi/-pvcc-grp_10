bf8865a6c5a8233f5bdd201f7c1c7ce6b5c9f9ee
Léo Fornoff <leo.fornoff@telecomnancy.eu>
2022-11-03 19:44:53 +0100

    web : login page responsive working

--- pp2i/Web/app.py
+++ pp2i/Web/app.py
@@ -11,6 +11,16 @@ def index():
 def navbar():
     return render_template("navBar.html")
 
+@app.route('/signin')
+def signin():
+    return render_template("login/signin.html")    
+@app.route('/signup')
+def signup():
+    return render_template("login/signup.html")   
+
+@app.route('/login')
+def login():
+    return render_template("login/login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
