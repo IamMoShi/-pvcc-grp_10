15653cedaa03557dc4fb496768c55c9e0c313edc
Léo Fornoff <leo.fornoff@telecomnancy.eu>
2022-11-16 22:13:15 +0100

    add autoincremendation (db) + begin add user in db

--- pp2i/Web/app.py
+++ pp2i/Web/app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,g
+import back_python.login.register as register_py
+import back_python.login.hash as hash_py
+import sqlite3
 
 app = Flask(__name__)
+
+DATABASE = 'database.db'
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/status')
@@ -21,6 +37,24 @@ def signup():
 @app.route('/login')
 def login():
     return render_template("login/login.html")
+
+@app.route('/send-register-form', methods = ['POST', 'GET'])
+def register_post():
+    if request.method == 'POST':
+        last_name = request.form['lastname']
+        first_name = request.form['firstname']
+        email = request.form['email']
+        password = request.form['password']
+        confirmation = request.form['confirmation']
+        validation = register_py.register(last_name, first_name, email, password, confirmation)
+        if validation[0]:
+            pwd_hash = hash_py.hash(password)
+            db = get_db()
+            items = db.cursor()
+            items.execute('INSERT INTO utilisateur (nom, prenom, mail, mdp) VALUES (?,?,?,?)', (last_name, first_name, email, pwd_hash))
+            db.commit()
+            return 'POST'
+    return 'GET'
 
 if __name__ == '__main__':
     app.run(debug=True)
