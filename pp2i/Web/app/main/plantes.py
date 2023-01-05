618ccf62a9c2be5dc34f48a45965d2c260b41a25
Léo Fornoff <leo.fornoff@telecomnancy.eu>
2023-01-05 18:29:57 +0100

    Création de la fonction amis_ennemis.py dans fonctions/plante/

--- pp2i/Web/app/main/plantes.py
+++ pp2i/Web/app/main/plantes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
 from ..database.get_db import get_db
-from ..fonctions.potager.amis_ennemis import amis_ennemies
+from ..fonctions.plante.amis_ennemis import amis_ennemis
 
 plante = Blueprint('plante', __name__)
 
@@ -9,29 +9,4 @@ plante = Blueprint('plante', __name__)
 @plante.route('/id_plante/<numero>')
 def id_plante(numero):
 
-    try:
-        int(numero)
-    except:
-        return render_template("error_page.html", msg="Une erreur concernant le numéro de la plante est survenue")
-
-    database = get_db()
-
-    items = database.cursor()
-    items.execute('select * from plante where id_plante = ?', (numero,))
-
-    resultat = items.fetchall()
-
-    if not resultat:
-        return render_template("error_page.html", msg="Aucune plante ne possède cet id")
-
-    id_plante, taille, nom, color = resultat[0]
-
-    items = database.cursor()
-    items.execute('Select * from compagnons where plante1 = ? or plante2 = ?', (id_plante, id_plante,))
-
-    amis = items.fetchall()
-
-    if not amis:
-        return render_template("error_page.html", msg="Une erreur est survenue")
-
-    amis = amis_ennemies(id_plante, amis)
+    return str(amis_ennemis(numero, get_db()))
