e4309b702be55da2177eb7b4f3ffd3fda2000cb9
Léo Fornoff <leo.fornoff@telecomnancy.eu>
2023-01-05 17:10:21 +0100

    Patch de l'affichage de la légende

--- pp2i/Web/app/fonctions/potager/legende_fonction.py
+++ pp2i/Web/app/fonctions/potager/legende_fonction.py
@@ -4,12 +4,12 @@ def legende_fonction(items, l_id):
     :return: la liste des couleurs et des noms associées à chaque plante présente dans le jardin
     """
     l_legende = [('grey', 0)]
-    l_id = []
+    l_id_uniq = []
 
     for id in l_id:
         items.execute('select color from plante where id_plante = ?', (int(id),))
         result = items.fetchall()
-        if len(result) == 1 and id not in l_id:
-            l_id.append(id)
+        if len(result) == 1 and id not in l_id_uniq:
+            l_id_uniq.append(id)
             l_legende.append((result[0][0], id))
     return l_legende
